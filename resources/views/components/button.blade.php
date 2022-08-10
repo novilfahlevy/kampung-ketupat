370b2bc72e9771921d35656ae7be148b8e4e5a99
novilfahlevy <novilfreon@gmail.com>
2022-08-10 21:24:10 +0800

    UNDO: buttons

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,7 +1,7 @@
 @props(['type' => 'button', 'color' => 'gray', 'icon' => '', 'class' => ''])
 
 @if ($color == 'gray')
-<button type="{{ $type }}" class="{{ 'inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150' }}">
+<button type="{{ $type }}" class="{{ 'inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150 '.$class }}">
     @if ($icon !== '')
     <i class="{{ $icon.' cursor-pointer select-none mr-3' }}"></i>
     @endif
@@ -10,7 +10,7 @@
 @endif
 
 @if ($color == 'green')
-<button type="{{ $type }}" class="{{ 'inline-flex items-center px-4 py-2 bg-green-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-green-700 active:bg-green-900 focus:outline-none focus:border-green-900 focus:ring ring-green-300 disabled:opacity-25 transition ease-in-out duration-150' }}">
+<button type="{{ $type }}" class="{{ 'inline-flex items-center px-4 py-2 bg-green-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-green-700 active:bg-green-900 focus:outline-none focus:border-green-900 focus:ring ring-green-300 disabled:opacity-25 transition ease-in-out duration-150 '.$class }}">
     @if ($icon !== '')
     <i class="{{ $icon.' cursor-pointer select-none mr-3' }}"></i>
     @endif
@@ -19,7 +19,7 @@
 @endif
 
 @if ($color == 'red')
-<button type="{{ $type }}" class="{{ 'inline-flex items-center px-4 py-2 bg-red-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-700 active:bg-red-900 focus:outline-none focus:border-red-900 focus:ring ring-red-300 disabled:opacity-25 transition ease-in-out duration-150' }}">
+<button type="{{ $type }}" class="{{ 'inline-flex items-center px-4 py-2 bg-red-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-700 active:bg-red-900 focus:outline-none focus:border-red-900 focus:ring ring-red-300 disabled:opacity-25 transition ease-in-out duration-150 '.$class }}">
     @if ($icon !== '')
     <i class="{{ $icon.' cursor-pointer select-none mr-3' }}"></i>
     @endif
